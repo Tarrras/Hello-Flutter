f968fd8a439be835e62d940e04b0702374a61455
Taras <vtaras15@gmail.com>
2020-01-28 12:11:05 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return RandomWordState();
+  }
+}
+
+class RandomWordState extends State<RandomWords> {
+  final List<WordPair> _suggestions = List<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  final Set<WordPair> _saved = Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildSuggestions(),
+      appBar: AppBar(
+        title: Text("StartupNameGeneration"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
+      ),
+    );
+  }
+
+  void _pushSaved() {}
+
+  Widget buildDivider() {
+    return Scaffold(
+      body: Text("Shit"),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(20),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider(
+            color: Colors.black26,
+            thickness: 2,
+            height: 20,
+            indent: 0,
+          );
+        }
+
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length && index <= 30) {
+          _suggestions.addAll(generateWordPairs().take(10));
+          //TODO: change
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
---
+++ lib/new_folder/new_file.dart
@@ -0,0 +1,2 @@
+//TODO: dsdsds
+//FIXME: DDD
